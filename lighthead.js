e472fac96ced2f5692cbb0de3f61cb03fda50442
Tinnaphat "Tin" Somsang <loyaltyplex@gmail.com>
2022-03-15 09:13:36 +0700

    Reformat files

--- lighthead.js
+++ lighthead.js
@@ -1,16 +1,24 @@
-let lighthead = (code, cb) => {
-  let memory = [0]
-  let isLoop, inner, pointer = 0;
+let lightHead = (code, cb) => {
+  let isLoop,
+    inner,
+    pointer = 0;
+  let memory = [0];
   let stack = [];
   let add = (i) => {
-    i ? pointer += i: '';
-    if (pointer >= memory.length) {
-      memory.push(0);
-    }
-  }
+    i ? (pointer += i) : "";
+    if (pointer >= memory.length) memory.push(0);
+  };
   for (let i = 0; i < code.length; i++) {
-    let char = code[i]
-    isLoop ? char === '[' ? inner++ : char === ']' ? inner === 0 ? isLoop = false : inner-- : '' : '';
+    let char = code[i];
+    isLoop
+      ? char === "["
+        ? inner++
+        : char === "]"
+        ? inner === 0
+          ? (isLoop = 0)
+          : inner--
+        : ""
+      : "";
     switch (char) {
       case "+":
         memory[pointer]++;
@@ -31,13 +39,13 @@ let lighthead = (code, cb) => {
         memory[pointer] = cb();
         break;
       case "[":
-        !memory[pointer] ?  isLoop = true : stack.push(i);
+        !memory[pointer] ? (isLoop = 1) : stack.push(i);
         break;
       case "]":
-        memory[pointer] ? i = stack[stack.length - 1] : stack.pop();
+        memory[pointer] ? (i = stack[stack.length - 1]) : stack.pop();
         break;
     }
   }
-}
+};
 
-module.exports = lighthead;
+module.exports = lightHead;
--- minify.js
+++ minify.js
@@ -1,8 +1,8 @@
 // Import Terser so we can use it
-const { minify } = require('terser');
+const { minify } = require("terser");
 
 // Import fs so we can read/write files
-const fs = require('fs');
+const fs = require("fs");
 
 // Define the config for how Terser should minify the code
 // This is set to how you currently have this web tool configured
@@ -14,28 +14,27 @@ const config = {
     keep_classnames: true,
     keep_fargs: true,
     keep_fnames: true,
-    keep_infinity: true
+    keep_infinity: true,
   },
   mangle: {
     eval: true,
     keep_classnames: true,
     keep_fnames: true,
     toplevel: true,
-    safari10: true
+    safari10: true,
   },
   module: false,
   sourceMap: false,
   output: {
-    comments: 'some'
-  }
+    comments: "some",
+  },
 };
 
 // Load in your code to minify
-const code = fs.readFileSync('lighthead.js', 'utf8');
+const code = fs.readFileSync("lighthead.js", "utf8");
 
 // Minify the code with Terser
-const minified = minify(code, config)
-  .then(result => {
-    // Write the minified code to a file
-    fs.writeFileSync('lighthead.min.js', result.code);
-  })
+const minified = minify(code, config).then((result) => {
+  // Write the minified code to a file
+  fs.writeFileSync("lighthead.min.js", result.code);
+});
